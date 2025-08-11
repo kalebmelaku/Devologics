acda7a85eabbf7e9ad8db9ab1badacc89fa33dbe
kalebmelaku <kalebmelaku97@hotmail.com>
2025-08-11 18:33:10 +0300

    hero section updated

--- src/components/Hero/middle.jsx
+++ src/components/Hero/middle.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import logo from '../../assets/logo.png';
+import React, { useEffect, useState } from 'react';
+import logo from '../../assets/logo.svg';
+import Threads from '../../libs/Threads';
+import TextType from '../../libs/TextType';
 function Middle()
 {
     return (
         <div className="max-w-screen-2xl w-full bg-dark h-[50vh] mx-auto rounded-[2rem] rounded-br-none flex items-center justify-between">
             <div className="px-8">
                 <h1 className='text-white text-7xl'>
-                    Logic Meets <br /> Innovation at <br /> <span className='text-secondary font-semibold'>Devologics</span> Software
+                    Logic Meets <br /> Innovation <span className='text-4xl'>at</span> <br />
+                    <TextType
+                        text={["Devologics", "Software"]}
+                        typingSpeed={100}
+                        pauseDuration={1500}
+                        showCursor={true}
+                        cursorCharacter="|"
+                        className='font-semibold '
+                        textColors={['#f36921', '#ffffff']}
+                        loop={true}
+                    />
                 </h1>
                 <p className='text-white text-xl mt-4 mb-12'>
                     At Devologics, we craft smart, scalable, and future-ready software solutions that merge cutting-edge technology with strategic thinking.
                 </p>
-                <a href="" className='bg-secondary text-white rounded-full px-6 py-3 hover:bg-primary transition-all duration-500'>Learn More</a>
+                <a href="" className='bg-gradient-to-r from-secondary via-[#f69b6d] to-secondary hover:from-primary hover:via-[#4576ca] hover:to-primary text-white rounded-full px-6 py-4 transition-all duration-500'>Learn More</a>
             </div>
-            <div className="w-1/2 bg-primary">
-                <img src={logo} alt="" />
+            <div className="w-full flex items-center justify-center h-full">
+                <Threads
+                    amplitude={4}
+                    distance={0}
+                    enableMouseInteraction={true}
+                />
             </div>
         </div>
     );
--- src/components/Hero/top.jsx
+++ src/components/Hero/top.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
 import { ArrowRight, Mail } from 'lucide-react';
+import GooeyNav from '../../libs/GooeyNav';
 function Top()
 {
+    const items = [
+        { label: "Home", href: "#" },
+        { label: "Services", href: "#" },
+        { label: "About", href: "#" },
+        { label: "Contact", href: "#" },
+    ];
     return (
         <div className="relative max-w-screen-2xl w-full bg-dark mx-auto mt-4">
             <div className='absolute top-0 left-0 w-full h-10 bg-white'></div>
@@ -11,15 +18,19 @@ function Top()
                     <img src={logo} alt="" className='w-1/2 p-2' />
                 </div>
                 <nav className='flex relative items-center h-20 justify-center rounded-tl-[2rem] rounded-tr-[2rem] bg-dark w-full'>
-                    <ul className='flex items-center space-x-6 text-lg font-semibold'>
-                        <li><a href="#" className="text-white hover:text-secondary">Home</a></li>
-                        <li><a href="#" className="text-white hover:text-secondary">About</a></li>
-                        <li><a href="#" className="text-white hover:text-secondary">Services</a></li>
-                        <li><a href="#" className="text-white hover:text-secondary">Contact</a></li>
-                    </ul>
+                    <GooeyNav
+                        items={items}
+                        particleCount={15}
+                        particleDistances={[90, 10]}
+                        particleR={100}
+                        initialActiveIndex={0}
+                        animationTime={600}
+                        timeVariance={300}
+                        colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+                    />
                 </nav>
                 <div className="bg-white rounded-bl-[2rem] w-48 h-full flex flex-col items-center justify-center">
-                    <a href='' className="bg-secondary hover:bg-primary text-white px-5 py-3 rounded-full flex items-center justify-center transition-all duration-300">
+                    <a href='' className="bg-gradient-to-r from-secondary via-[#f69b6d] to-secondary hover:bg-gradient-to-r hover:from-primary hover:via-[#4576ca] hover:to-primary text-white px-5 py-4 rounded-full flex items-center justify-center transition-all duration-300">
                         Contact Us
                         <ArrowRight />
                     </a>
